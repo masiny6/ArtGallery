653a5a69af5730309b4d34b4165c3f0125dcc308
masiny6 <suvorkin_2001@mail.ru>
2020-11-14 10:51:02 +0300

    Очередные изменения

---
+++ app/components/product-card-catalog/product-card-catalog.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.product-card .swiper-container', {
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+        clickable: true,
+        el: '.swiper-pagination'
+    },
+    loop: true
+});
