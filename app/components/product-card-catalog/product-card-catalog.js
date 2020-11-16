a0be490a05a2be997630bd0b2fa16d0cd6154365
masiny6 <suvorkin_2001@mail.ru>
2020-11-16 20:27:42 +0300

    Desktop вёрстка карточки товара завершена

--- app/components/product-card-catalog/product-card-catalog.js
+++ app/components/product-card-catalog/product-card-catalog.js
@@ -1,17 +1,35 @@
-var swiper = new Swiper('.product-card .swiper-container-one', {
-    pagination: {
-        clickable: true,
-        el: '.swiper-pagination'
-    },
-    loop: true,
+var swiperProduct = new Swiper('.product-card .swiper-container-one', {
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    // loop: true,
     direction: "vertical",
     slidesPerView: 6,
-    spaceBetween: 10,
+    spaceBetween: 10
 });
 var swiper = new Swiper('.product-card .swiper-container-two', {
-    pagination: {
-        clickable: true,
-        el: '.swiper-pagination'
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-    loop: true
+    loop: true,
+    thumbs: {
+        swiper: swiperProduct
+      }
+});
+$(".product-card .swiper-button_next").on("click", function() {
+    $(".product-card .swiper-button-next").click();
+});
+$(".product-card .swiper-button_prev").on("click", function() {
+    $(".product-card .swiper-button-prev").click();
+});
+$(".parameters-picture__button").on("click", function() {
+    $(".parameters-picture .tab-arrow").toggleClass("js-tab-arrow");
+    $(".parameters-table").toggleClass("js-parameters-table");
+    $(".parameters-picture__button").toggleClass("js-parameters-picture__button");
+});
+$(".description-picture__button").on("click", function() {
+    $(".description-picture .tab-arrow").toggleClass("js-tab-arrow");
+    $(".description-picture__text").toggleClass("js-description-picture__text");
 });
