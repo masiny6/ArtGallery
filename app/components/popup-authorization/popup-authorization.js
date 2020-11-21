9bf9e45ce9185d048bfe7c3c2d3500eb53c11302
masiny6 <suvorkin_2001@mail.ru>
2020-11-21 17:38:20 +0300

    Закончены все макеты Desktop'a

---
+++ app/components/popup-authorization/popup-authorization.js
@@ -0,0 +1,3 @@
+$(".popup-authorization .social-network__elem").on("click", function() {
+$(".popup-authorization .social-network__link").click();
+});
---
+++ app/components/popup-registration/popup-registration.js
@@ -0,0 +1,3 @@
+$(".popup-registration .social-network__elem").on("click", function() {
+    $(".popup-registration .social-network__link").click();
+    });
